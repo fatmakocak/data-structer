ac23516fc89a88f39813e22c6a521f37dd3f0cae
FATMA KOÇAK <42037891+ftmkck66@users.noreply.github.com>
2020-11-25 13:19:24 +0300

    Update applett.java

--- applett.java
+++ applett.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package odev2v2;
+package odev2;
 
 import java.applet.Applet;
 import java.awt.Graphics;
